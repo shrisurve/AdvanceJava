939092a44ad921a6d4a979b3a27ea93052aef15e
shrisurve <119746964+shrisurve@users.noreply.github.com>
2024-03-28 13:12:19 +0530

    Create AdvanceJava1.java

---
+++ AdvanceJava1.java
@@ -0,0 +1,8 @@
+import java.lang.*;'
+public class Test
+  {
+    public static void main(String[]args)
+    {
+      System.out.println("Heyyy Hiii Welcome In GitHub Advance Java Repository");
+    }
+  }
