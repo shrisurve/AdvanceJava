7ef5d7a61d8226fe25262190ec4ce188a2666a40
shrisurve <119746964+shrisurve@users.noreply.github.com>
2024-03-28 13:21:32 +0530

    Added Scanner Class AdvanceJava1.java

--- AdvanceJava1.java
+++ AdvanceJava1.java
@@ -1,8 +1,10 @@
 import java.lang.*;'
+import java.util.*;
 public class Test
   {
     public static void main(String[]args)
     {
+      Scanner scanner=new System(System.in);
       System.out.println("Heyyy Hiii Welcome In GitHub Advance Java Repository");
     }
   }
